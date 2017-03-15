6642e57dba3d5c8839d0c95451727bbcd6fdfacd
Debby Rachmania <debbyrachmaniap@gmail.com>
2017-03-15 11:33:50 -0400

    scaffold schedule subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170315152703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "schedules", force: :cascade do |t|
+    t.string   "title"
+    t.date     "date"
+    t.string   "archive"
+    t.text     "description"
+    t.boolean  "approve"
+    t.integer  "subject_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["subject_id"], name: "index_schedules_on_subject_id", using: :btree
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "schedules", "subjects"
 end
